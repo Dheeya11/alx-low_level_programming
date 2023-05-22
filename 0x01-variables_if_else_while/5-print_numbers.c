11a4366afb16dfd8060dff1e0d485c7a2e804ae5
Dheeya11 <mudiyaoyiza02@gmail.com>
2023-05-22 08:15:14 -0700

    writing a program that prints all single digit numbers of base 10 starting from 0

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,14 +7,16 @@
 * Return: always return 0 (Success)
 */
 
-int main(void) {
-    char c;
+int main(void)
+{
+	 char c;
 
-    for (c = '0'; c <= '9'; ++c) {
-        putchar(c);
-    }
+	for (c = '0'; c <= '9'; ++c)
+{
+		putchar(c);
+	}
 
-    putchar('\n');
+	putchar('\n');
 
-    return 0;
+	return (0);
 }
