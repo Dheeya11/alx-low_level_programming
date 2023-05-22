c4f055fd89de867f0743e2306fc52af6adf990a7
Dheeya11 <mudiyaoyiza02@gmail.com>
2023-05-22 12:35:05 -0700

    writing all possible combinations of two two-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,21 +12,25 @@ int main(void)
 	int num1, num2;
 
 	for (num1 = 0; num1 < 100; num1++)
-
+	{
 		for (num2 = num1; num2 < 100; num2++)
-
+		{
 			if (num1 != num2)
-
+			{
 				if (num1 < num2)
-
+				{
 					printf("%02d %02d", num1, num2);
-				else
+				} else
+					{
 					printf("%02d %02d", num2, num1);
-
+				}
 			if (num1 != 98 || num2 != 99)
-
+						{
 				printf(", ");
-
-		printf("\n");
-		return (0);
+				}
+			}
+		}
+	}
+	printf("\n");
+	return (0);
 }
