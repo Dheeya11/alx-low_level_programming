93f52b18bda60afaf636393179af6c6e4be2a2b3
Dheeya11 <mudiyaoyiza02@gmail.com>
2023-05-30 15:50:50 -0700

    Generating passwords

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,24 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
  * main - generates keygen.
  * Return: 0 Always.
  */
+
 int main(void)
 {
-	int r = 0, c = 0;
-	time_t t;
+        int r = 0, c = 0;
+        time_t t;
 
-	srand((unsigned int) time(&t));
-	while (c < 2772)
-	{
-		r = rand() % 128;
-		if ((c + r) > 2772)
-			break;
-		c = c + r;
-		printf("%c", r);
-	}
-	printf("%c\n", (2772 - c));
-	return (0);
+        /* Seed the random number generator */
+        srand((unsigned int)time(&t));
+
+        /* Generate characters until the total count reaches 2772 */
+        while (c < 2772)
+        {
+                r = rand() % 94 + 33; /* Generate a random number between 33 and>
+                if ((c + r) > 2772)
+                        break;
+                c = c + r;
+                printf("%c", r); /* Print the generated character */
+        }
+
+        /* Print the remaining difference to reach 2772 */
+        printf("%c\n", (2772 - c));
+
+        return (0);
 }
